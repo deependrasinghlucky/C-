69a357e2928eb9e21dc9574a1ae15b7fd488985d
Deependra Singh <108728913+deependrasinghlucky@users.noreply.github.com>
2023-06-26 15:17:32 +0530

    Add files via upload

---
+++ Rev 1 for loop break continue  prime non prime xor.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+//     // const int area=1000;
+//     // cout<<area;
+
+// //for loop
+// // for(initilization; condition; updation)
+
+
+// for(int i=1; i<n; i++){
+// cout<<i;
+
+// }
+
+
+
+//continue 
+// for(int i=1; i<n; i++){
+//     if(i==4 || i==6){
+//     continue;
+//     }
+// cout<<i<<" ";
+
+// }
+//
+
+
+//break
+
+
+// for(int i=1; i<n; i++){
+//     if(i==4){
+//     break;
+//     }
+// cout<<i<<" ";
+
+// }
+
+
+// prime not prime
+
+// int i=2;
+// while(i<n){
+//     if(n%i==0){
+
+//         cout<<"not prime"<<" ";
+//         break;
+    
+//     }
+//     i++;
+
+//     if(i==n){
+//         cout<<"prime";
+
+//     }
+
+// }
+
+//
+//XOR to print unique no
+// int no;
+// cin>>no;
+// int ans=no;
+
+// int i=1;
+// while(i<n){
+// cin>>no;
+// ans=ans^no;
+// i++;
+
+// }
+// cout<<"unique no is "<<ans;
+
+
+
+
+
+    return 0;
+
+
+}
